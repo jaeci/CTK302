bd5e2a7a7c377dee5367ab68cbb61dddad21524b
jaeci <64815708+jaeci@users.noreply.github.com>
2021-04-30 21:45:08 -0500

    4

--- Unit4/gyro/sketch.js
+++ Unit4/gyro/sketch.js
@@ -21,7 +21,7 @@ function setup() {
 
 function draw() {
 
-  background('#c6f5ff'); // light blue
+  background('#608f61'); // green
 
   // the map command !!!!
   // takes your variable and maps it from range 1 to range 2
@@ -41,27 +41,27 @@ function draw() {
 
 
   // Text commands that display debugging data
-  textAlign(LEFT);
-  textSize(20);
-  fill('black');
-  text("orientation data:", 25, 25);
-  textSize(15);
-  text("alpha: " + alpha, 25, 50);
-  text("beta: " + beta, 25, 70);
-  text("gamma: " + gamma, 25, 90);
-  textSize(20);
-  text("acceleration data:", 25, 125);
-  textSize(15);
-  text("x = " + x.toFixed(2), 25, 150); // .toFixed means just show (x) decimal places
-  text("y = " + y.toFixed(2), 25, 170);
-  text("z = " + z.toFixed(4), 25, 190);
+  // textAlign(LEFT);
+  // textSize(20);
+  // fill('black');
+  // text("orientation data:", 25, 25);
+  // textSize(15);
+  // text("alpha: " + alpha, 25, 50);
+  // text("beta: " + beta, 25, 70);
+  // text("gamma: " + gamma, 25, 90);
+  // textSize(20);
+  // text("acceleration data:", 25, 125);
+  // textSize(15);
+  // text("x = " + x.toFixed(2), 25, 150); // .toFixed means just show (x) decimal places
+  // text("y = " + y.toFixed(2), 25, 170);
+  // text("z = " + z.toFixed(4), 25, 190);
 
   // Text that makes CTK type in the background
-  fill('white');
+  fill('#27308a');//dark blue
   noStroke();
-  textSize(300);
+  textSize(50);
   textAlign(CENTER);
-  text("ctk", width / 2, height / 2);
+  text("moobloom", width / 2, height / 2);
 
 }
 
